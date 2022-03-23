36959200e928ebc72f3734a71a0f4809faebc79f
lukastan93 <lukastan93@gmail.com>
2022-03-23 14:11:49 +0400

    Update main.py
    Updated bot code

--- bot/main.py
+++ bot/main.py
@@ -1,16 +1,66 @@
+import discord
 import os
-from discord.ext import commands
+import requests
+import json
+import re
+import random
+from keep_alive import keep_alive
+client = discord.Client()
 
-bot = commands.Bot(command_prefix="!")
-TOKEN = os.getenv("DISCORD_TOKEN")
+def process_commands(message):
+    response = requests.get("https://api.opensea.io/api/v1/collection/everyday-goddesses")
+    dump = json.loads(response.text)
 
-@bot.event
+    content = message[1:]
+    if content.split(" ")[0] in ["stats", "floor"]:
+      embed=discord.Embed(title="Everyday Goddesses Stats", url="https://opensea.io/collection/everyday-goddesses/", description="Collection statistics for Everyday Goddesses.", color=discord.Color.blue())
+      embed.set_author(name="MiniMort", url="https://twitter.com/nft_shiny", icon_url="https://i.imgur.com/7TkJmoi.png")
+      embed.set_thumbnail(url="https://i.imgur.com/gPflHCF.png")
+      embed.add_field(name="Floor Price", value="{}".format(str(dump["collection"]["stats"]["floor_price"]) + "Ξ"), inline=False)
+      embed.add_field(name="1-Day Volume", value="{}".format(str(round(dump["collection"]["stats"]["one_day_volume"], 4)) + "Ξ"), inline=True)
+      embed.add_field(name="1-Day Sales", value="{}".format(str(int(dump["collection"]["stats"]["one_day_sales"]))), inline=True)
+      embed.add_field(name="Holder Count", value="{}".format(str(dump["collection"]["stats"]["num_owners"])), inline=True)
+      return embed
+
+    if content.split(" ")[0] == "rank":
+      embed=discord.Embed(color=discord.Color.blue())
+      embed.set_thumbnail(url="https://images.raritysniffer.com/500/500/ipfs/QmcKNVfrYpwYemYUFzBgbMwLaJEDj7MGaiU1BAoxDgESyx/{}.png".format(content.split(" ")[1]))
+      embed.add_field(name="Token ID", value="{}".format(content.split(" ")[1]))
+
+      response = requests.get("https://raritysniffer.dev/api/v1/collection?collection=0x9176e11a412b6ef5e8ddb045909a14112f7782b2&norm=true&traitCount=true&partial=false").json()
+
+      for i in response["data"]:
+        if i["id"] == int(content.split(" ")[1]):
+          embed.add_field(name="Rank", value="{}".format(i["positionId"]))
+      return embed
+
+    if content.split(" ")[0] == "gas":
+      embed=discord.Embed(title="Gas Tracker ⛽", url="https://etherscan.io/gastracker", description="Current gas prices on the ETH network.", color=0xffd500)
+      page = requests.get("https://api.etherscan.io/api?module=gastracker&action=gasoracle").json()
+      slow = page["result"]["SafeGasPrice"]
+      medium = page["result"]["ProposeGasPrice"]
+      fast = page["result"]["FastGasPrice"]
+      embed.add_field(name="Slow", value="{}".format(slow), inline=True)
+      embed.add_field(name="Medium", value="{}".format(medium), inline=True)
+      embed.add_field(name="Fast", value="{}".format(fast), inline=True)
+      return embed
+
+
+@client.event
 async def on_ready():
-    print(f"Logged in as {bot.user.name}({bot.user.id})")
+    print('We have logged in as {0.user}'.format(client))
 
-@bot.command()
-async def ping(ctx):
-    await ctx.send("pong")
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
 
-if __name__ == "__main__":
-    bot.run(TOKEN)
+    if message.content.startswith('$'):
+        result = process_commands(message.content)
+        await message.channel.send(embed = result)
+
+    else:
+      return "Invalid command."
+
+keep_alive()
+client.run(os.getenv('TOKEN'))
